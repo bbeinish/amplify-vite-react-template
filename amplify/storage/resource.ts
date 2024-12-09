48e6e928af94f0bb51d0d6293791462bff1c801c
Benny Beinish <bbeinish@edci.com>
2024-12-09 14:36:15 -0600

    Trying again to provision basic server resources

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,9 +1,9 @@
 import { defineStorage } from "@aws-amplify/backend";
-import { downloadClip } from "../functions/downloadClip/resource";
+// import { downloadClip } from "../functions/downloadClip/resource";
 
 export const storage = defineStorage({
   name: "myReports",
-  access: (allow) => ({
-    "reports/*": [allow.resource(downloadClip).to(["read", "write", "delete"])],
-  }),
+  // access: (allow) => ({
+  //   "reports/*": [allow.resource(downloadClip).to(["read", "write", "delete"])],
+  // }),
 });
