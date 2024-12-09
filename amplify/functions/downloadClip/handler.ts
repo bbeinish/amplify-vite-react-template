f9d0a4a16d3f2f8d22c1930887eb74a58c90bbc7
Benny Beinish <bbeinish@edci.com>
2024-12-09 17:28:26 -0600

    Trying to upload to s3

--- amplify/functions/downloadClip/handler.ts
+++ amplify/functions/downloadClip/handler.ts
@@ -1,8 +1,13 @@
 import { Schema } from "../../data/resource";
+import { uploadData } from "@aws-amplify/storage";
 
 export const handler: Schema["downloadClip"]["functionHandler"] = async (
   event
 ) => {
   console.log(event.arguments.videoUrl);
+  uploadData({
+    path: "savedClips/test",
+    data: "test",
+  });
   return true;
 };
