9f7a28efc17b2d5a940c54770178ba791b7bd6bf
Benny Beinish <bbeinish@edci.com>
2024-12-10 08:57:41 -0600

    Trying to upload a video

--- amplify/functions/downloadClip/handler.ts
+++ amplify/functions/downloadClip/handler.ts
@@ -1,13 +1,38 @@
 import { Schema } from "../../data/resource";
 import { uploadData } from "@aws-amplify/storage";
+import ytdl from "ytdl-core";
+import ffmpeg from "fluent-ffmpeg";
+import { PassThrough } from "stream";
 
 export const handler: Schema["downloadClip"]["functionHandler"] = async (
   event
 ) => {
   console.log(event.arguments.videoUrl);
+  const { videoUrl, startTime, endTime } = event.arguments;
+  const videoStream = ytdl(videoUrl!, {
+    quality: "highest",
+    range: {
+      start: startTime!,
+      end: endTime!,
+    },
+  });
+
+  const passThrough = new PassThrough();
+  ffmpeg(videoStream).toFormat("mp4").pipe(passThrough);
+
+  const chunks: Uint8Array[] = [];
+  for await (const chunk of passThrough) {
+    chunks.push(chunk);
+  }
+  const ret = Buffer.concat(chunks);
+
   uploadData({
     path: "savedClips/test",
-    data: "test",
+    data: ret,
+    options: {
+      contentType: "video/mp4",
+    },
   });
+
   return true;
 };
