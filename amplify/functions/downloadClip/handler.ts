9b647f652c88daaab18961026243127804ffd752
Benny Beinish <bbeinish@edci.com>
2024-12-10 11:56:12 -0600

    Trying a different library

--- amplify/functions/downloadClip/handler.ts
+++ amplify/functions/downloadClip/handler.ts
@@ -1,32 +1,37 @@
 import { Schema } from "../../data/resource";
 import { uploadData } from "@aws-amplify/storage";
-import ytdl from "ytdl-core";
+// import ytdl from "ytdl-core";
+import { createReadStream } from "fs";
 // import ffmpegPath from "@ffmpeg-installer/ffmpeg";
 // import ffmpeg from "fluent-ffmpeg";
 // import { PassThrough } from "stream";
+import youtubeDl from "youtube-dl-exec";
 
 export const handler: Schema["downloadClip"]["functionHandler"] = async (
   event
 ) => {
   // ffmpeg.setFfmpegPath(ffmpegPath.path);
   console.log(event.arguments.videoUrl);
-  const { videoUrl, startTime, endTime } = event.arguments;
-  const videoStream = ytdl(videoUrl!, {
-    quality: "highest",
-    range: {
-      start: startTime!,
-      end: endTime!,
-    },
+  const { videoUrl } = event.arguments;
+
+  const outputPath = "savedClips/test";
+  await youtubeDl(videoUrl!, {
+    format: "mp4",
+    output: outputPath,
   });
 
-  console.log("Video Stream", videoStream);
+  console.log("Output Path", outputPath);
 
   // const passThrough = new PassThrough();
   // const streamResult = ffmpeg(videoStream).toFormat("mp4").pipe(passThrough);
   // console.log(streamResult);
 
+  const stream = createReadStream(outputPath);
+
+  console.log("Stream", stream);
+
   const chunks: Uint8Array[] = [];
-  for await (const chunk of videoStream) {
+  for await (const chunk of stream) {
     chunks.push(chunk);
   }
   const ret = Buffer.concat(chunks);
