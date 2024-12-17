fe788708dc589e28db93a71cd72973c246be21d0
Benny Beinish <bbeinish@edci.com>
2024-12-17 15:47:01 -0600

    Trying to create the lambda layer

--- amplify/functions/downloadClip/resource.ts
+++ amplify/functions/downloadClip/resource.ts
@@ -3,4 +3,7 @@ import { defineFunction } from "@aws-amplify/backend";
 export const downloadClip = defineFunction({
   name: "downloadClip",
   entry: "./handler.ts",
+  layers: {
+    arn: "arn:aws:lambda:us-east-1:145266761615:layer:ffmpeg:1",
+  },
 });
