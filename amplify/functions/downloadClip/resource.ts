eeb557f67c5c34c4bd3d195009a7c1fcfa55c248
Benny Beinish <bbeinish@edci.com>
2024-12-17 16:32:29 -0600

    Using next version of lambda layer

--- amplify/functions/downloadClip/resource.ts
+++ amplify/functions/downloadClip/resource.ts
@@ -5,6 +5,6 @@ export const downloadClip = defineFunction({
   entry: "./handler.ts",
   layers: {
     "youtube-dl-exec":
-      "arn:aws:lambda:us-east-2:277707106912:layer:firstTryLayer:1",
+      "arn:aws:lambda:us-east-2:277707106912:layer:firstTryLayer:2",
   },
 });
