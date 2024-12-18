431701cdc3777cd3be2f0c2da78f85b3dc3f1bc6
Benny Beinish <bbeinish@edci.com>
2024-12-18 13:51:52 -0600

    Using newest version of lambda layer. This one tries to import yt-dlp

--- amplify/functions/downloadClip/resource.ts
+++ amplify/functions/downloadClip/resource.ts
@@ -4,6 +4,6 @@ export const downloadClip = defineFunction({
   name: "downloadClip",
   entry: "./handler.ts",
   layers: {
-    youtubeDl: "arn:aws:lambda:us-east-2:277707106912:layer:firstTryLayer:3",
+    youtubeDl: "arn:aws:lambda:us-east-2:277707106912:layer:firstTryLayer:4",
   },
 });
