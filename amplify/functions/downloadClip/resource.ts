506fcf27b1281cc75e63d117fd30f3b4c159179d
Benny Beinish <bbeinish@edci.com>
2024-12-18 13:24:20 -0600

    Trying to change the layer import

--- amplify/functions/downloadClip/resource.ts
+++ amplify/functions/downloadClip/resource.ts
@@ -4,7 +4,6 @@ export const downloadClip = defineFunction({
   name: "downloadClip",
   entry: "./handler.ts",
   layers: {
-    "youtube-dl-exec":
-      "arn:aws:lambda:us-east-2:277707106912:layer:firstTryLayer:3",
+    youtubeDl: "arn:aws:lambda:us-east-2:277707106912:layer:firstTryLayer:3",
   },
 });
